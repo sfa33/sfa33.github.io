089ffe63246ea638c6ce6b1b9eac6e3580df8f93
Sara Frandatte Alshalabi <sfa@Saras-MacBook-Pro.local>
2022-01-06 17:20:17 +0100

    wayyyy better

--- scratch.js
+++ scratch.js
@@ -10,12 +10,24 @@ function setup() {
     myCanvas.parent("canvas");
   }
 
+  function movingLines(strw) {
+    for (var y = 0; y <= height; y++) {
+      if (y % (strw * 2) == 0) {
+        strokeWeight(strw);
+        stroke(255);
+        line(0, y, mouseX, y);
+      }
+    }
+    return 0;
+  } 
+
 function draw() {
     background(0);
     strokeWeight(100);
     
         var x = 0;
         var y = 0;
+
         for (var y = 0; y <= width/50; y += 1) {
           for (var x = 0; x <= width/50; x += 1) {
             if (x % 2 == 0){
@@ -31,45 +43,52 @@ function draw() {
           }  
         }
 
+movingLines(30);
 
 
 
 
+        // var words = ["rainbow", "heart","purple", "friendship", "love"];
 
-      //   var words = ["rainbow", "heart","purple", "friendship", "love"];
+        // var index = 0;
 
-      //   var index = 0;
-
-
-      //   fill(255);
-      //   textSize(32);
-      //   text(words[index], 300, 300); 
+        strokeWeight(0);
+        textSize(40);
+        text(words[index], width / 2, height / 2);
         
-      // }
       
-      // function mouseDown() {
+      // function mousePressed() {
       //   index = index + 1;
         
       //   if (index == words.length) {
       //     index = 0;
 
 
-      //   }
-
-
-        
-
-
-      background(255,255,255,1);
-      strokeWeight(4);
-      stroke(255);
-    
-      for (var x = 0; x <= mouseX; x++) {
-        for (var y = 0; y <= width; y++) {
-          fill(255);
-          ellipse(x * 1, y * 50, 25, 25);
         }
+
+      
+        
+    
+        var words = ["rainbow", "heart","purple", "friendship", "love"];
+
+        var index = 0;
+
+        // strokeWeight(0);
+        // textSize(40);
+        // text(words[index], width / 2, height / 2);
+    
+    function mousePressed() {
+      index = index + 1;
+      
+      if (index == words.length) {
+       index = 0;
+
+      console.log ("hey i pressed the fuck u")
+
       }
+
+    }
+
 
 // for (let i = 0; i < 500; i++) {
 //   let r = random(-1000, 1000);
@@ -81,4 +100,7 @@ function draw() {
 //   }
 
 
-    }
+    
+
+
+      
